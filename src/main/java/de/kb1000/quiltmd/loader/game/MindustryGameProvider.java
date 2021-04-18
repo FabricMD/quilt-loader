FabricMD/fabric-loader

Initial version of Mindustry support code

src/main/java/de/kb1000/fabricmd/loader/game/MindustryGameProvider.java
@@ -1,0 +1,171 @@
+package de.kb1000.fabricmd.loader.game;
+
+import de.kb1000.fabricmd.loader.entrypoint.mindustry.EntrypointPatchPlatform;
+import de.kb1000.fabricmd.loader.mindustry.MindustryVersionLookup;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.loader.entrypoint.EntrypointTransformer;
+import net.fabricmc.loader.game.GameProvider;
+import net.fabricmc.loader.game.GameProviderHelper;
+import net.fabricmc.loader.metadata.BuiltinModMetadata;
+import net.fabricmc.loader.util.SystemProperties;
+
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class MindustryGameProvider implements GameProvider {
+	private EnvType envType;
+	private String entrypoint;
+	private MindustryVersionLookup.MindustryVersion versionData;
+	private Path gameJar;
+
+	private Path launchDirectory;
+	private String[] arguments;
+
+	public static final EntrypointTransformer TRANSFORMER = new EntrypointTransformer(it -> Arrays.asList(
+			new EntrypointPatchPlatform(it)
+	));
+
+	@Override
+	public String getGameId() {
+		return "mindustry";
+	}
+
+	@Override
+	public String getGameName() {
+		return "Mindustry";
+	}
+
+	@Override
+	public String getRawGameVersion() {
+		return versionData.raw;
+	}
+
+	@Override
+	public String getNormalizedGameVersion() {
+		return versionData.normalized;
+	}
+
+	@Override
+	public Collection<BuiltinMod> getBuiltinMods() {
+		URL url;
+
+		try {
+			url = gameJar.toUri().toURL();
+		} catch (MalformedURLException e) {
+			throw new RuntimeException(e);
+		}
+
+		return Collections.singletonList(
+				new BuiltinMod(url, new BuiltinModMetadata.Builder(getGameId(), getNormalizedGameVersion())
+						.setName(getGameName())
+						.build())
+		);
+	}
+
+	@Override
+	public String getEntrypoint() {
+		return entrypoint;
+	}
+
+	@Override
+	public Path getLaunchDirectory() {
+		if (launchDirectory != null) {
+			return launchDirectory;
+		} else {
+			return launchDirectory = getLaunchDirectoryImpl();
+		}
+	}
+
+	private Path getLaunchDirectoryImpl() {
+		// Right now, this is a reimplementation of what's found in arc.util.OS#getAppDataDirectoryString,
+		// without the dependencies, and directly returning a Path. This is needed because this is called
+		// just after the GameProvider is loaded (no references to the game code permitted).
+		// The API appears to have been designed for Minecraft, where the directory is easily statically
+		// determinable (game argument or current directory).
+		// FIXME: add support for Steam
+		final String osName = System.getProperty("os.name");
+		if (osName.contains("Windows")) {
+			return Paths.get(System.getenv("AppData"), "Mindustry");
+		} else if (osName.contains("Linux")) {
+			if (System.getenv("XDG_DATA_HOME") != null) {
+				return Paths.get(System.getenv("XDG_DATA_HOME"), "Mindustry");
+			} else {
+				return Paths.get(System.getProperty("user.home"), ".local", "share", "Mindustry");
+			}
+		} else if (osName.contains("Mac")) {
+			return Paths.get(System.getProperty("user.home"), "Library", "Application Support", "Mindustry");
+		} else {
+			throw new RuntimeException("Unknown operating system");
+		}
+	}
+
+	@Override
+	public boolean isObfuscated() {
+		return false;
+	}
+
+	@Override
+	public boolean requiresUrlClassLoader() {
+		return false;
+	}
+
+	@Override
+	public List<Path> getGameContextJars() {
+		return Collections.singletonList(gameJar);
+	}
+
+
+	@Override
+	public boolean locateGame(EnvType envType, String[] args, ClassLoader loader) {
+		this.envType = envType;
+		this.arguments = args;
+
+		List<String> entrypointClasses;
+
+		if (envType == EnvType.CLIENT) {
+			entrypointClasses = Collections.singletonList("mindustry.desktop.DesktopLauncher");
+		} else {
+			entrypointClasses = Collections.singletonList("mindustry.server.ServerLauncher");
+		}
+
+		Optional<GameProviderHelper.EntrypointResult> entrypointResult = GameProviderHelper.findFirstClass(loader, entrypointClasses);
+
+		if (!entrypointResult.isPresent()) {
+			return false;
+		}
+
+		entrypoint = entrypointResult.get().entrypointName;
+		gameJar = entrypointResult.get().entrypointPath;
+
+		String version = System.getProperty(SystemProperties.GAME_VERSION);
+		versionData = version != null ? MindustryVersionLookup.getVersion(version) : MindustryVersionLookup.getVersion(gameJar);
+
+		return true;
+	}
+
+	@Override
+	public EntrypointTransformer getEntrypointTransformer() {
+		return TRANSFORMER;
+	}
+
+	@Override
+	public void launch(ClassLoader loader) {
+		try {
+			Class<?> c = loader.loadClass(entrypoint);
+			Method m = c.getMethod("main", String[].class);
+			m.invoke(null, (Object) arguments);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public String[] getLaunchArguments(boolean sanitize) {
+		// TODO: does this work?
+		return new String[0];
+	}
+}

src/main/java/de/kb1000/fabricmd/loader/mindustry/MindustryVersionLookup.java
@@ -1,0 +1,57 @@
+package de.kb1000.fabricmd.loader.mindustry;
+
+import net.fabricmc.loader.minecraft.McVersionLookup;
+import net.fabricmc.loader.util.FileSystemUtil;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Properties;
+
+public class MindustryVersionLookup {
+	private static final String DEFAULT_MAJOR_VERSION = "4";
+	private static final String DEFAULT_BUILD_NUMBER = "-1";
+
+	public static MindustryVersion getVersion(Path gameJar) {
+		try (FileSystemUtil.FileSystemDelegate jarFs = FileSystemUtil.getJarFileSystem(gameJar, false)) {
+			FileSystem fs = jarFs.get();
+			Path file;
+
+			// version.properties - contains major version number and build number
+			if (Files.isRegularFile(file = fs.getPath("version.properties"))) {
+				return fromVersionProperties(Files.newInputStream(file));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return new MindustryVersion(DEFAULT_MAJOR_VERSION, DEFAULT_BUILD_NUMBER);
+	}
+
+	private static MindustryVersion fromVersionProperties(InputStream inputStream) throws IOException {
+		Properties properties = new Properties();
+		properties.load(inputStream);
+		return new MindustryVersion(properties.getProperty("number", DEFAULT_MAJOR_VERSION), properties.getProperty("build", DEFAULT_BUILD_NUMBER));
+	}
+
+	public static MindustryVersion getVersion(String gameVersion) {
+		String[] version = gameVersion.split("\\.", 1);
+		return new MindustryVersion(version[0], version[1]);
+	}
+
+	public static final class MindustryVersion {
+		private MindustryVersion(String number, String version) {
+			this.raw = version;
+			this.normalized = number + "." + version;
+		}
+
+		@Override
+		public String toString() {
+			return String.format("%s/%s", raw, normalized);
+		}
+
+		public final String raw; // raw version, e.g. 18w12a
+		public final String normalized; // normalized version, usually Semver compliant version containing release and pre-release as applicable
+	}
+}

src/main/java/net/fabricmc/loader/game/GameProviders.java
@@ -16,13 +16,15 @@
 
 package net.fabricmc.loader.game;
 
-import java.util.Collections;
+import de.kb1000.fabricmd.loader.game.MindustryGameProvider;
+
+import java.util.Arrays;
 import java.util.List;
 
 public final class GameProviders {
 	private GameProviders() { }
 
 	public static List<GameProvider> create() {
-		return Collections.singletonList(new MinecraftGameProvider());
+		return Arrays.asList(new MinecraftGameProvider(), new MindustryGameProvider());
 	}
 }

FabricMD/quilt-loader

src/main/java/de/kb1000/quiltmd/loader/game/MindustryGameProvider.java
@@ -1,0 +1,171 @@
+package de.kb1000.quiltmd.loader.game;
+
+import de.kb1000.quiltmd.loader.entrypoint.mindustry.EntrypointPatchPlatform;
+import de.kb1000.quiltmd.loader.mindustry.MindustryVersionLookup;
+import net.fabricmc.api.EnvType;
+import org.quiltmc.loader.impl.entrypoint.EntrypointTransformer;
+import org.quiltmc.loader.impl.game.GameProvider;
+import org.quiltmc.loader.impl.game.GameProviderHelper;
+import org.quiltmc.loader.impl.metadata.BuiltinModMetadata;
+import org.quiltmc.loader.impl.util.SystemProperties;
+
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class MindustryGameProvider implements GameProvider {
+	private EnvType envType;
+	private String entrypoint;
+	private MindustryVersionLookup.MindustryVersion versionData;
+	private Path gameJar;
+
+	private Path launchDirectory;
+	private String[] arguments;
+
+	public static final EntrypointTransformer TRANSFORMER = new EntrypointTransformer(it -> Arrays.asList(
+			new EntrypointPatchPlatform(it)
+	));
+
+	@Override
+	public String getGameId() {
+		return "mindustry";
+	}
+
+	@Override
+	public String getGameName() {
+		return "Mindustry";
+	}
+
+	@Override
+	public String getRawGameVersion() {
+		return versionData.raw;
+	}
+
+	@Override
+	public String getNormalizedGameVersion() {
+		return versionData.normalized;
+	}
+
+	@Override
+	public Collection<BuiltinMod> getBuiltinMods() {
+		URL url;
+
+		try {
+			url = gameJar.toUri().toURL();
+		} catch (MalformedURLException e) {
+			throw new RuntimeException(e);
+		}
+
+		return Collections.singletonList(
+				new BuiltinMod(url, new BuiltinModMetadata.Builder(getGameId(), getNormalizedGameVersion())
+						.setName(getGameName())
+						.build())
+		);
+	}
+
+	@Override
+	public String getEntrypoint() {
+		return entrypoint;
+	}
+
+	@Override
+	public Path getLaunchDirectory() {
+		if (launchDirectory != null) {
+			return launchDirectory;
+		} else {
+			return launchDirectory = getLaunchDirectoryImpl();
+		}
+	}
+
+	private Path getLaunchDirectoryImpl() {
+		// Right now, this is a reimplementation of what's found in arc.util.OS#getAppDataDirectoryString,
+		// without the dependencies, and directly returning a Path. This is needed because this is called
+		// just after the GameProvider is loaded (no references to the game code permitted).
+		// The API appears to have been designed for Minecraft, where the directory is easily statically
+		// determinable (game argument or current directory).
+		// FIXME: add support for Steam
+		final String osName = System.getProperty("os.name");
+		if (osName.contains("Windows")) {
+			return Paths.get(System.getenv("AppData"), "Mindustry");
+		} else if (osName.contains("Linux")) {
+			if (System.getenv("XDG_DATA_HOME") != null) {
+				return Paths.get(System.getenv("XDG_DATA_HOME"), "Mindustry");
+			} else {
+				return Paths.get(System.getProperty("user.home"), ".local", "share", "Mindustry");
+			}
+		} else if (osName.contains("Mac")) {
+			return Paths.get(System.getProperty("user.home"), "Library", "Application Support", "Mindustry");
+		} else {
+			throw new RuntimeException("Unknown operating system");
+		}
+	}
+
+	@Override
+	public boolean isObfuscated() {
+		return false;
+	}
+
+	@Override
+	public boolean requiresUrlClassLoader() {
+		return false;
+	}
+
+	@Override
+	public List<Path> getGameContextJars() {
+		return Collections.singletonList(gameJar);
+	}
+
+
+	@Override
+	public boolean locateGame(EnvType envType, String[] args, ClassLoader loader) {
+		this.envType = envType;
+		this.arguments = args;
+
+		List<String> entrypointClasses;
+
+		if (envType == EnvType.CLIENT) {
+			entrypointClasses = Collections.singletonList("mindustry.desktop.DesktopLauncher");
+		} else {
+			entrypointClasses = Collections.singletonList("mindustry.server.ServerLauncher");
+		}
+
+		Optional<GameProviderHelper.EntrypointResult> entrypointResult = GameProviderHelper.findFirstClass(loader, entrypointClasses);
+
+		if (!entrypointResult.isPresent()) {
+			return false;
+		}
+
+		entrypoint = entrypointResult.get().entrypointName;
+		gameJar = entrypointResult.get().entrypointPath;
+
+		String version = System.getProperty(SystemProperties.GAME_VERSION);
+		versionData = version != null ? MindustryVersionLookup.getVersion(version) : MindustryVersionLookup.getVersion(gameJar);
+
+		return true;
+	}
+
+	@Override
+	public EntrypointTransformer getEntrypointTransformer() {
+		return TRANSFORMER;
+	}
+
+	@Override
+	public void launch(ClassLoader loader) {
+		try {
+			Class<?> c = loader.loadClass(entrypoint);
+			Method m = c.getMethod("main", String[].class);
+			m.invoke(null, (Object) arguments);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public String[] getLaunchArguments(boolean sanitize) {
+		// TODO: does this work?
+		return new String[0];
+	}
+}

src/main/java/de/kb1000/quiltmd/loader/mindustry/MindustryVersionLookup.java
@@ -1,0 +1,56 @@
+package de.kb1000.quiltmd.loader.mindustry;
+
+import org.quiltmc.loader.impl.util.FileSystemUtil;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Properties;
+
+public class MindustryVersionLookup {
+	private static final String DEFAULT_MAJOR_VERSION = "4";
+	private static final String DEFAULT_BUILD_NUMBER = "-1";
+
+	public static MindustryVersion getVersion(Path gameJar) {
+		try (FileSystemUtil.FileSystemDelegate jarFs = FileSystemUtil.getJarFileSystem(gameJar, false)) {
+			FileSystem fs = jarFs.get();
+			Path file;
+
+			// version.properties - contains major version number and build number
+			if (Files.isRegularFile(file = fs.getPath("version.properties"))) {
+				return fromVersionProperties(Files.newInputStream(file));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return new MindustryVersion(DEFAULT_MAJOR_VERSION, DEFAULT_BUILD_NUMBER);
+	}
+
+	private static MindustryVersion fromVersionProperties(InputStream inputStream) throws IOException {
+		Properties properties = new Properties();
+		properties.load(inputStream);
+		return new MindustryVersion(properties.getProperty("number", DEFAULT_MAJOR_VERSION), properties.getProperty("build", DEFAULT_BUILD_NUMBER));
+	}
+
+	public static MindustryVersion getVersion(String gameVersion) {
+		String[] version = gameVersion.split("\\.", 1);
+		return new MindustryVersion(version[0], version[1]);
+	}
+
+	public static final class MindustryVersion {
+		private MindustryVersion(String number, String version) {
+			this.raw = version;
+			this.normalized = number + "." + version;
+		}
+
+		@Override
+		public String toString() {
+			return String.format("%s/%s", raw, normalized);
+		}
+
+		public final String raw; // raw version, e.g. 18w12a
+		public final String normalized; // normalized version, usually Semver compliant version containing release and pre-release as applicable
+	}
+}

src/main/java/org/quiltmc/loader/impl/game/GameProviders.java
@@ -16,13 +16,15 @@
 
 package org.quiltmc.loader.impl.game;
 
-import java.util.Collections;
+import de.kb1000.quiltmd.loader.game.MindustryGameProvider;
+
+import java.util.Arrays;
 import java.util.List;
 
 public final class GameProviders {
 	private GameProviders() { }
 
 	public static List<GameProvider> create() {
-		return Collections.singletonList(new MinecraftGameProvider());
+		return Arrays.asList(new MinecraftGameProvider(), new MindustryGameProvider());
 	}
 }